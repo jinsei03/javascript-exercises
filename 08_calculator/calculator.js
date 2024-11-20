7377c325c2a83543d04972ee4061e62d9392b212
yami <qjasonc@gmail.com>
2024-11-19 22:36:03 -0800

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,47 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 
 };
 
-const power = function() {
+const power = function(num, pow) {
+  const arr = [];
+  for(let i = 0; i < pow; i++){
+    arr.push(num);
+  }
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  const arr = [];
+  for(let i = 0; i < num; i++)
+  {
+    arr.push(num - i);
+  }
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 	
 };
 
