1a64cce0e90a2c9e222e7471f97d634f1e768e5e
yami <qjasonc@gmail.com>
2024-11-19 23:09:15 -0800

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuationless = string.replace(/[^\w\s]|\s/g,"");
+    punctuationless = punctuationless.toLowerCase();
+    const arr = punctuationless.split('');
+    let palindrome = arr.reverse();
+    palindrome = palindrome.join('');
+    return palindrome === punctuationless;
 
 };
 
