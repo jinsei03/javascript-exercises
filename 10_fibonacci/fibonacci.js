c0ea6f5687efd7866705e20ec486ec205ea6e57f
yami <qjasonc@gmail.com>
2024-11-19 23:36:02 -0800

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num < 0)
+    {
+        return "OOPS";
+    }
+    else if(num <= 1)
+    {
+        return num;
+    }
+    else
+    {
+        let fib = 0;
+        let a = 0;
+        let b = 1;
+        for(let i = 1; i < num; i++)
+        {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+        return fib;
+    }
+    
 
 };
 
