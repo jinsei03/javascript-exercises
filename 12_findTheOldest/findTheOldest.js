26220af36dfed2762d4f3479aa5cf5d097761207
yami <qjasonc@gmail.com>
2024-11-20 00:32:09 -0800

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let ages = [];
+    for(let i = 0; i < array.length ;i++){
+            if(array[i].yearOfDeath == undefined)
+            {
+                array[i].yearOfDeath =  new Date().getFullYear();
+            }
+            ages.push(Math.abs(array[i].yearOfDeath - array[i].yearOfBirth));
+            
+        }
+    let index = 0;
+    let max = 0;
+    for(let i = 0; i < array.length; i++)
+    {
+        if(ages[max] < ages[i])
+        {
+            max = i;
+            index = ages.indexOf(ages[i]);
+        }
+    }
+    return array[index];
 };
 
 // Do not edit below this line
